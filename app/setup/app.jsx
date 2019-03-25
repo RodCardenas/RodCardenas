d2ffb9fadfe8740a5c2d0e46ce93cf5fd73152ac
Rodrigo Cardenas <rodrigo.cardenas@rigminder.com>
2019-03-25 17:33:39 -0500

    Get a hi rendering on page.

--- app/setup/app.jsx
+++ app/setup/app.jsx
@@ -1,40 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
-import { HashRouter, Switch } from 'react-router-dom';
 import { MuiThemeProvider } from '@material-ui/core/styles';
-import Loadable from 'react-loadable';
-import { CircularProgress } from '@material-ui/core';
 
 import theme from '../util/theme';
 
 import './reset.css';
 import './layout.css';
 
-const loadingStyle = {
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: 'calc(100vh - 140px)',
-};
-
-const Loading = () => (
-  <div style={loadingStyle}>
-    <CircularProgress />
-  </div>
-);
-
 const App = ({ store }) => (
   <Provider store={store}>
     <MuiThemeProvider theme={theme}>
-      <HashRouter>
-        <div className="layout-area">
-          <Switch>
-            <Route path="/" component={<div>Hi!</div>}/>
-          </Switch>
-          <Notifier />
-        </div>
-      </HashRouter>
+      <div className="layout-area">Hi!</div>
     </MuiThemeProvider>
   </Provider>
 );
