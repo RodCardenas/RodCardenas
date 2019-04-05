4b416d2df605e0198b7fe87965f19109de8d8aed
Rodrigo Cardenas <rodrigo.cardenas@rigminder.com>
2019-04-05 13:38:30 -0500

    Table names will be singular.

--- src/api/posts.js
+++ src/api/posts.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var queryDB = require("./databaseInterface.js");
 
 router.get('/', function (req, res) {
-  queryDB('Select * from posts;', req, res);
+  queryDB('Select * from post;', req, res);
 });
 
 module.exports = router;
